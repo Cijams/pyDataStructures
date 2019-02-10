f44491862d430bb9369fa59ea835dce111efdeb3
Cijams <cijams@asu.edu>
2019-02-10 14:10:59 -0800

    Implemented 3 methods.
    Implemented Contains(), Add(), and Size. to the linkedlist.

--- LinkedList/PyLinkedList.py
+++ LinkedList/PyLinkedList.py
@@ -1,42 +1,27 @@
 class PyLinkedList:
-    def __init__(self):
-        self.head = self.Node(None)
+    def __init__(self, head=None):
+        self.head = head
 
     def add(self, data=None):
-        if not self.head.data:
-            self.head.data = data
-            return "First Time"
-        else:
-            count = 0
-            print(self.head.data)
-            print(self.head.get_next())
-            print(self.head.set_next(data))
-            print(self.head.get_next())
-            self.head = self.head.get_next
-            print(self.head.data)
-            """
-            while self.head.get_next() is not None:
-                count = count+1
-                print(self.head.get_data())
-                self.head = self.head.get_next
-                print("yep")
-            self.head.get_next = self.Node(data)
-            """
-            return count
+        temp_node = self.Node(data)
+        temp_node.set_next(self.head)
+        self.head = temp_node
 
+    def size(self):
+        count = 0
+        temp = self.head
+        while temp is not None:
+            temp = temp.get_next()
+            count = count + 1
+        return count
 
-
-
-        # True/False return
-
-    def add_first(self):
-        pass
-
-    def add_last(self):
-        pass
-
-    def contains(self):
-        pass
+    def contains(self, data):
+        temp = self.head
+        while temp is not None:
+            if temp.get_data() == data:
+                return True
+            temp = temp.get_next()
+        return False
 
     def get(self):
         pass
@@ -47,16 +32,25 @@ class PyLinkedList:
     def get_last(self):
         pass
 
+    def iterate(self):
+        node_list = []
+        temp = self.head
+        while temp is not None:
+            node_list.append(temp.get_data())
+            temp = temp.get_next()
+        node_list.reverse()
+        return node_list
+
     class Node:
-        def __init__(self, data=None, next_node=None):
-            self.data = data
-            self.next_node = next_node
+        def __init__(self, _data=None):
+            self._data = _data
+            self._next = None
 
         def get_data(self):
-            return self.data
+            return self._data
 
         def get_next(self):
-            return self.next_node
+            return self._next
 
-        def set_next(self, next_node=None):
-            self.next_node = next_node
+        def set_next(self, _next=None):
+            self._next = _next
--- LinkedList/PyLinkedListTest.py
+++ LinkedList/PyLinkedListTest.py
@@ -9,7 +9,12 @@ class MyTestCase(unittest.TestCase):
     def test_add(self):
         pl.add(1)
         pl.add(2)
+        pl.add(3)
+        pl.add(5)
 
+        print(pl.iterate())
+        print(pl.size())
+        print(pl.contains(2))
 
 if __name__ == '__main__':
     unittest.main()
