8bf58fd0afa4c411eda987f3751863de86fbd198
Christopher <Cocopelly255@gmail.com>
2019-02-12 17:43:56 -0800

    Updated comments
    Added small descriptors to each method.

--- Queue/PyQueue.py
+++ Queue/PyQueue.py
@@ -2,20 +2,26 @@ class PyQueue:
     def __init__(self):
         self.queue = []
 
+    # Push an item onto the queue.
     def push(self, item):
         self.queue.insert(0, item)
 
+    # Remove an element from the queue.
     def deque(self):
         return self.queue.pop() if self.queue else IndexError
 
+    # Returns the next item to be removed.
     def peek(self):
         return self.queue[-1] if self.queue else IndexError
 
+    # Returns the size of the queue.
     def size(self):
         return len(self.queue)
 
+    # Returns True if the queue is empty.
     def is_empty(self):
         return False if self.queue else True
 
+    # Removes all item from the queue.
     def clear(self):
         self.queue.clear()
--- Stack/PyStack.py
+++ Stack/PyStack.py
@@ -2,20 +2,26 @@ class PyStack:
     def __init__(self):
         self.stack = []
 
+    # Push an item onto the stack.
     def push(self, item):
         self.stack.append(item)
 
+    # Remove an element from the stack.
     def pop(self):
         return self.stack.pop() if self.stack else TypeError
 
+    # Returns the next item to be removed.
     def peek(self):
         return self.stack[-1] if self.stack else TypeError
 
+    # Returns True if the stack is empty.
     def is_empty(self):
         return False if self.stack else True
 
+    # Returns the size of the stack.
     def size(self):
         return len(self.stack)
 
+    # Removes all item from the stack.
     def clear(self):
         self.stack.clear()
