d3c89245e12413b58eea818db9c6a1ae084c4016
Christopher <Cocopelly255@gmail.com>
2019-02-15 14:07:38 -0800

    Insert and IO Traverse added
    Added methods to insert into the binary tree and traverse the tree.
    
    Cleaned up a naming convention on LinkedList.

--- BinaryTree/PyBinaryTreeTest.py
+++ BinaryTree/PyBinaryTreeTest.py
@@ -7,7 +7,11 @@ bt = PyBinaryTree.PyBinaryTree()
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        pass
+        bt.insert(5)
+        bt.insert(3)
+        bt.insert(1)
+        bt.insert(6)
+        bt.in_order_traversal()
 
 
 if __name__ == '__main__':
