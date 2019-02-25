ade619c842e1f1421fd8651b088afd6c9d9c3dca
Christopher <Cocopelly255@gmail.com>
2019-02-25 15:08:08 -0800

    Implemented Hashtable - Chaining
    Implemented a simple version of a hash table using the chaining method.

--- pyHashTable/PyHashTable.py
+++ pyHashTable/PyHashTable.py
@@ -1,44 +1,75 @@
 class PyHashTable:
     def __init__(self):
         self.table = tuple([] for _ in range(16))
+        self.size = 0
 
     # Adds an element into the hash table.
     def add(self, data):
-        self.table[self._hash(data)].append(data)
+        try:
+            my_hash = self._hash(data)
+            if not self.contains(data):
+                self.size += 1
+                self.table[my_hash].append(data)
+        except TypeError as e:
+            return e
 
     # Hash algorithm for picking where to go.
     @staticmethod
     def _hash(data):
-        return ~(hash(data) * 11) % 16
+        try:
+            my_hash = 0
+            if type(data) == str or type(data) == chr:
+                for _ in data:
+                    my_hash = (my_hash + ord(_)) % 512
+            elif type(data) == int:
+                my_hash = data * 256
+            elif type(data) == float:
+                my_hash = int((512 + data) // 10)
+            else:
+                raise AttributeError
+            return (~my_hash * 11) % 16
+        except AttributeError as e:
+            return e
 
     # Retrieves the value from the hash table.
-    def get(self):
-        pass
+    def get(self, data):
+        if self.contains(data):
+            temp = self.table[self._hash(data)]
+            for _ in temp:
+                if _ == data:
+                    return data
 
     # Removes the value from the hash table.
-    def remove(self):
-        pass
+    def remove(self, data):
+        if self.contains(data):
+            temp = self.table[self._hash(data)]
+            index = 0
+            while index < len(temp):
+                if temp[index] == data:
+                    del temp[index]
+                    self.size -= 1
+                    break
+                index += 1
 
     # Removes all data from the hash table.
     def clear(self):
-        pass
+        self.table = tuple([] for _ in range(16))
 
-    # Returns an exact copy of the hash table.
-    def clone(self):
-        pass
-
-    # Returns True if the vale is within the hashtable.
-    def contains(self):
-        pass
-
-    # Generates a hash code for the given value.
-    def hash_code(self):
-        pass
+    # Returns True if the vale is within the hash table.
+    def contains(self, data):
+        try:
+            temp = self.table[self._hash(data)]
+            for _ in temp:
+                if _ == data:
+                    return True
+            return False
+        except TypeError as e:
+            return e
 
     # Returns the size of the hash table.
-    def size(self):
-        pass
+    def get_size(self):
+        return self.size
 
-    # Prints the hash table
-    def print_table(self):
-        print(self.table)
+    # Returns the hash table
+    def get_table(self):
+        return self.table
--- pyHashTable/PyHashTableTest.py
+++ pyHashTable/PyHashTableTest.py
@@ -5,16 +5,35 @@ pt = PyHashTable()
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
-        pt.add(5)
-        pt.add('r')
-        pt.add(333)
+    def test_hash_table(self):
+        pt.add("5")
+        pt.add(4)
+        pt.add(4)
+        pt.add(4.6)
+        pt.add("hello, chris")
+        pt.add(2)
+        pt.add(33)
+        pt.get_table()
+        self.assertEqual(pt.table, ([], [], ['hello, chris'], [], [4.6], [4, 2, 33], [], [], [], [], [], [], [], [],
+                                    ['5'], []))
+        self.assertTrue(pt.contains(4))
+        self.assertFalse(pt.contains(3))
+        self.assertEqual(pt.get(4), 4)
+        self.assertIsNone(pt.get(500))
 
-        print(hash('r'))
-        print(hash('r'))
+        self.assertEqual(pt.get_size(), 6)
+        pt.remove(4)
+        self.assertEqual(pt.table, ([], [], ['hello, chris'], [], [4.6], [2, 33], [], [], [], [], [], [], [], [],
+                                    ['5'], []))
+        self.assertEqual(pt.get_size(), 5)
+        self.assertEqual(pt.get_table(), pt.table)
+        pt.clear()
+        self.assertEqual(pt.table, ([], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []))
 
-        pt.print_table()
+        pt.add(None)
+        pt.add(bool)
+        self.assertEqual(pt.table, ([], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []))
+
 
 if __name__ == '__main__':
     unittest.main()
