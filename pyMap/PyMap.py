a1a4e2694bd0887b6ba4be1b1e31b1e5e610d21f
Christopher <cocopelly255@gmail.com>
2019-03-01 15:40:00 -0800

    Updated Map functionality

--- pyMap/PyMap.py
+++ pyMap/PyMap.py
@@ -11,43 +11,107 @@ class PyMap:
 
     def __str__(self):
         hm = ""
-        while self.head:
-            hm += "{" + str(self.head.key) + ": " + str(self.head.value) + "}"
-            hm += ", "
-            self.head = self.head.next
-        hm = hm[:-2]
-        return hm
+        curr = self.head
+        while curr:
+            hm += "{" + str(curr.key) + ": " + str(curr.value) + "}, "
+            curr = curr.next
+        return hm[:-2]
 
-    def __iter__(self):
-        pass
-
-    def __next__(self):
-        pass
-
+    # Add a key-value pair into the map.
     def put(self, key=None, value=None):
         if self.head:
-            curr = self._Node(key, value)
-            curr.next = self.head
-            self.head = curr
+            if self.retrieve(key):
+                curr = self.retrieve(key)
+                curr.value = value
+            else:
+                curr = self._Node(key, value)
+                curr.next = self.head
+                self.head = curr
         else:
             self.head = self._Node(key, value)
 
-
-    def get(self):
-        pass
-
+    # Returns a list of the keys in the map.
     def keys(self):
-        pass
+        curr = self.head
+        keys = []
+        while curr:
+            keys.append(curr.key)
+            curr = curr.next
+        return keys
 
+    # Returns a list of the values in the map.
     def values(self):
-        pass
+        curr = self.head
+        values = []
+        while curr:
+            values.append(curr.value)
+            curr = curr.next
+        return values
 
+    # Removes the last key-value pair in the map.
     def pop(self):
-        pass
+        if self.head.next is None:
+            self.head = None
+        else:
+            curr = self.head
+            while curr.next.next:
+                curr = curr.next
+            curr.next = None
 
-    def remove(self):
-        pass
+    # Returns the position of the key-value pair
+    def retrieve(self, key):
+        if self.head.key == key:
+            return self.head
+        else:
+            curr = self.head
+            while curr.next:
+                if curr.next.key == key:
+                    return curr.next
+                else:
+                    curr = curr.next
+        return False
 
+    # Returns True if the map contains the key
+    def contains_key(self, key):
+        if self.head.key == key:
+            return True
+        else:
+            curr = self.head
+            while curr.next:
+                if curr.next.key == key:
+                    return True
+                else:
+                    curr = curr.next
+        return False
+
+    # Returns True if the map contains the value
+    def contains_value(self, value):
+        if self.head.value == value:
+            return True
+        else:
+            curr = self.head
+            while curr.next:
+                if curr.next.value == value:
+                    return True
+                else:
+                    curr = curr.next
+        return False
+
+    # Removes a value and key from the map with the given key.
+    def remove_key(self, key):
+        if self.head.key == key:
+            self.head = self.head.next
+        else:
+            curr = self.head
+            while curr.next:
+                if curr.next.key == key:
+                    curr.next = curr.next.next
+                    return True
+                else:
+                    curr = curr.next
+        return False
+
+    # Internal node class to store key-value pairs.
     class _Node:
         def __init__(self, key=None, value=None):
             self.next = None
--- pyMap/PyMapTest.py
+++ pyMap/PyMapTest.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 from pyMap import PyMap
 
 
@@ -6,11 +7,26 @@ pm = PyMap.PyMap()
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        pass
-    pm.put(5, 6)
-    pm.put(7, 9)
-    print(pm)
+    def test_map(self):
+        pm.put(5, 6)
+        pm.put(7, 9)
+        self.assertTrue(pm, ({7: 9}, {5: 6}))
+        self.assertEqual(pm.keys(), [7, 5])
+        self.assertEqual(pm.values(), [9, 6])
+        pm.pop()
+        self.assertTrue(pm)
+        pm.pop()
+        self.assertEqual(pm.__str__(), "")
+
+        for e in range(5):
+            pm.put(e, e+1)
+
+        print(pm)
+        pm.remove_key(4)
+        print(pm)
+        pm.contains_value(4)
+        self.assertTrue(pm.contains_value(4))
+        self.assertTrue(pm.contains_key(3))
 
 
 if __name__ == '__main__':
